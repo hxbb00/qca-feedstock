178b7e3d3bca74947d7ba08af8b6f0b11fb82263
dbyte <hxbb00@qq.com>
2026-06-17 12:18:34 +0800

    try split packages

--- recipe/build_qt6.sh
+++ recipe/build_qt6.sh
@@ -5,14 +5,19 @@ cd build/
 
 # "default" channel "qt" creates "plugins" in root of environment
 # Need to put "qca" plugin -- including qca-ossl -- in that folder
-cmake ${CMAKE_ARGS} \
+export VERBOSE=1
+cmake --trace ${CMAKE_ARGS} \
+    -G Ninja \
     -D CMAKE_INSTALL_PREFIX=${PREFIX} \
     -D USE_RELATIVE_PATHS=FALSE \
-    -D QCA_PLUGINS_INSTALL_DIR=${PREFIX}/plugins \
+    -D QCA_PLUGINS_INSTALL_DIR=${PREFIX}/lib/qt6/plugins \
     -D CMAKE_PREFIX_PATH=${PREFIX} \
     -D CMAKE_BUILD_TYPE=Release \
-    ${SRC_DIR}
+    -D BUILD_WITH_QT6=ON \
+    -D QCA_SUFFIX=qt6 \
+    -D BUILD_TESTS=OFF \
+    ${SRC_DIR} || (cat CMakeFiles/CMakeConfigureLog.yaml; false)
 
-make -j${CPU_COUNT}
+ninja -j${CPU_COUNT}
 # No make check
-make install
+ninja install
